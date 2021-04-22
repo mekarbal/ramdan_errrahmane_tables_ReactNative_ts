8db56af7efa08674808d4c5b8343084b5872e727
Elmahdi KARBAL <56792446+mekarbal@users.noreply.github.com>
2021-04-22 15:52:11 +0000

    list/addPlace/addBreakFast

--- app/assistances/services/assistance.service.tsx
+++ app/assistances/services/assistance.service.tsx
@@ -1,17 +1,17 @@
-import firebase from "firebase/app";
-import "firebase/auth";
-import "firebase/firestore";
+import firebase from "firebase";
 const db = firebase.firestore();
 
 
 
-const  add=(desc:string,long:number,lat:number)=>{
+const  add=(desc:string,long:number,lat:number,city:string,places:number)=>{
     if (db) {
         db.collection("assistancePlace")
           .add({
             description: desc,
             longitude: long,
             latitude: lat,
+            city: city,
+            places: places,
           })
          
       }
